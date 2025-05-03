d34bfd066fd8d69bacecf0d0941452460424ccad
gilmerperez <gilmer2002@outlook.com>
2025-05-03 19:28:16 -0400

    Set up JS file

---
+++ script.js
@@ -0,0 +1,47 @@
+// Text Area
+const textarea = document.getElementById("textarea");
+
+// Functional Buttons
+const clearBtn = document.getElementById("clearBtn");
+const lowercaseBtn = document.getElementById("lowercaseBtn");
+const titlecaseBtn = document.getElementById("titlecaseBtn");
+const uppercaseBtn = document.getElementById("uppercaseBtn");
+const undoBtn = document.getElementById("undoBtn");
+const redoBtn = document.getElementById("redoBtn");
+
+// Count Displays
+const characterCount = document.getElementById("characterCount");
+const wordCount = document.getElementById("wordCount");
+const lineCount = document.getElementById("lineCount");
+
+// Functions
+function clear() {}
+
+function lowercase() {}
+
+function titlecase() {}
+
+function uppercase() {}
+
+function undo() {}
+
+function redo() {}
+
+function characterCount() {}
+
+function wordCount() {}
+
+function lineCount() {}
+
+// Event Listeners
+clearBtn.addEventListener("click", clear);
+
+lowercaseBtn.addEventListener("click", lowercase);
+
+titlecaseBtn.addEventListener("click", titlecase);
+
+uppercaseBtn.addEventListener("click", uppercase);
+
+undoBtn.addEventListener("click", undo);
+
+redoBtn.addEventListener("click", redo);
